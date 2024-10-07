180f4fa3e71d196edb0ca60d4b4dd4e997fa1b9d
Sawachika <sawachika@orangestone.co>
2024-10-07 17:37:47 +0800

    Auth, Metadata

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,9 +2,14 @@ import Form from '@/app/ui/invoices/edit-form'
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs'
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data'
 import { notFound } from 'next/navigation'
+import { Metadata } from 'next'
 
-export default async function Page({ params }: { params: { id: string } }) {
-  const id = params.id
+export const metadata: Metadata = {
+  title: 'Invoices Edit',
+}
+
+export default async function Page({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params
   const [invoice, customers] = await Promise.all([
     fetchInvoiceById(id),
     fetchCustomers(),
--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,5 +1,7 @@
 'use server'
 
+import { signIn } from '@/auth'
+import { AuthError } from 'next-auth'
 import { z } from 'zod'
 import { sql } from '@vercel/postgres'
 import { revalidatePath } from 'next/cache'
@@ -15,6 +17,25 @@ const FormSchema = z.object({
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true })
 const UpdateInvoice = FormSchema.omit({ id: true, date: true })
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData)
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+      case 'CredentialsSignin':
+        return 'Invalid credentials.'
+      default:
+        return 'Something went wrong.'
+      }
+    }
+    throw error
+  }
+}
 
 export type State = {
   errors?: {
@@ -47,7 +68,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
       INSERT INTO invoices (customer_id, amount, status, date)
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `
-  } catch (error) {
+  } catch {
     return {
       message: 'Database Error: Failed to Create Invoice.',
     }
@@ -84,7 +105,7 @@ export async function updateInvoice(
         SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
         WHERE id = ${id}
       `
-  } catch (error) {
+  } catch {
     return { message: 'Database Error: Failed to Update Invoice.' }
   }
 
@@ -97,7 +118,7 @@ export async function deleteInvoice(id: string) {
     await sql`DELETE FROM invoices WHERE id = ${id}`
     revalidatePath('/dashboard/invoices')
     return { message: 'Deleted Invoice.' }
-  } catch (error) {
+  } catch {
     return { message: 'Database Error: Failed to Delete Invoice.' }
   }
 }
